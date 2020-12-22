2935b0211b7c7d767bf26bb401c98b5408ce0e3d
ahmedhabeeb73 <alnaib888@gmail.com>
2020-12-22 18:39:21 +0100

    add diffForHumans for date

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,12 +9,12 @@
                 @foreach($posts as $post)
                 <div class="flex-job">
                         <div class="flex-item p-2 border-b-2 rounded-b-sm">
-                            
+
                         <a class="btn" href="{{route('posts.show',$post)}}">
                             <h1>{{$post->title}}</h1>
                         </a>
                         
-                        <span class="text-gray-600 text-sm">{{$post->created_at}}</span>
+                        <span class="text-gray-600 text-sm">{{$post->created_at->diffForHumans()}}</span>
 
                         <a class="bg-green-200 text-grey px-6 py-1 rounded font-medium " href="{{route('posts.show',$post)}}">Apply</a>
                          
